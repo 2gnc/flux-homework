5d8589d5055df2e949e017cda0a8d8bc741e9a3f
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-29 22:40:42 +0300

    настроила сборку webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/  
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    host: '0.0.0.0',
+    port: 5555
+  }
+};
