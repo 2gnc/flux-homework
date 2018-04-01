22c840bac5dc329287f9bb44ec121f81193f3f8c
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-01 16:54:25 +0300

    initial commit

---
+++ src/utils/SendToServer.js
@@ -0,0 +1,20 @@
+export default class SendToServer {
+  sendData(data) {
+    console.log(data);
+    const event = new CustomEvent('dataIsSent', {
+      detail: data,
+    });
+    document.dispatchEvent(event); 
+  }
+}
+
+
+// Пример обработки события dataIsSent. Рекомендуется изменить API модуля так,
+// чтобы вызова события через document не было
+
+// function test() {
+//     document.addEventListener('dataIsSent', function(event) {
+//         console.log('event got ' + event.detail);
+//     });
+//     sendToServer('mydata');
+// }
