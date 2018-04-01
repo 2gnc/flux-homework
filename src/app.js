ebb6c5ec89ea2dcbfc5f85be241dd61bca1f6e41
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-01 20:45:52 +0300

    создан диспатчер и хранилище

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-import {Dispatcher, Action} from './MyFlux';
-import Sender from './utils/SendToServer';
+//import Sender from './utils/SendToServer';
+import Store from './MyStore';
+import Dispatcher from './MyDispatcher';
+
+const appDispatcher = new Dispatcher();
+const appStore = new Store();
+
+appDispatcher.register(appStore);
+
+
+console.log(appStore);
