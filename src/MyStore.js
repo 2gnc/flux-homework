6d836bf984e28be7c57147b142c79eed67d3fd32
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-02 10:36:11 +0300

    метод - заглушка

--- src/MyStore.js
+++ src/MyStore.js
@@ -3,5 +3,22 @@ import Store from './LittleFlux/Store';
 export default class MyStore extends Store {
   constructor() {
     super();
+    this.state = {
+      log: [],
+    }
   }
+
+  test() {
+    console.log('test');
+  }
+
+
 }
+
+
+
+// умеет подписываться на события (унаследовано)
+// умеет обрабатывать данные
+// умеет обрабатывать бизнес - логику
+// умеет оповещать о изменениях... вьюшки и диспетчер
+// хранит состояние приложения 
