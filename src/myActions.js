19bbda7517bd03d8c312b631afb12e4ce00c4617
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-02 17:11:02 +0300

    добавила тип экшена и экшен

--- src/myActions.js
+++ src/myActions.js
@@ -2,6 +2,12 @@ import actionTypes from './myActionsTypes';
 import {logger} from './LittleFlux/utils/logger'
 
 const Actions = {
+
+  /**
+   * @name sendData
+   * @param {Dispatcher} dispatcher 
+   * @param {String} text 
+   */
   sendData(dispatcher, text) {
     dispatcher.dispatch({
       type: actionTypes.DATA_SEND,
@@ -10,12 +16,32 @@ const Actions = {
     logger(`Actions: данные отправлены (${text})`);
   },
 
+/**
+ * @name getData
+ * @param {Dispatcher} dispatcher 
+ * @param {String} response 
+ */
   getData(dispatcher, response) {
     dispatcher.dispatch({
       type: actionTypes.RESPONCE_RECEIVED,
       response,
     });
-  }
+  },
+
+/**
+ * 
+ * @param {Dispatcher} dispatcher 
+ * @param {String} message 
+ * @param {Number} type 
+ */
+  log(dispatcher, message, type) {
+    dispatcher.dispatch({
+      type: actionTypes.MAKE_LOG,
+      message,
+      type
+    })
+  }, 
+
 }
 
 export default Actions;
--- src/myActionsTypes.js
+++ src/myActionsTypes.js
@@ -1,6 +1,7 @@
 const ActionTypes = {
   DATA_SEND: 'DATA_SEND',
   RESPONCE_RECEIVED: 'RESPONCE_RECEIVED',
+  MAKE_LOG: 'MAKE_LOG',
 };
 
 export default ActionTypes;
