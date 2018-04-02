a67ee90824a93402272946f46a6826bbaf29f564
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-02 10:36:32 +0300

    подписка на соытия

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-//import Sender from './utils/SendToServer';
 import Store from './MyStore';
 import Dispatcher from './MyDispatcher';
+import actions from './myActions';
+import types from './myActionsTypes';
 
 const appDispatcher = new Dispatcher();
 const appStore = new Store();
 
 appDispatcher.register(appStore);
+
+// подписались на события
+appStore.on(types.DATA_SEND, appStore.test);
+appStore.on(types.RESPONCE_RECEIVED, appStore.test);
+
 
 
 console.log(appStore);
